5839e84113357092387e55d3d12ddc79f597e161
thienkhoi280501 <thienkhoi2805@gmail.com>
2024-07-19 15:51:54 +0700

    Update file

--- application/views/admin/views/files.php
+++ application/views/admin/views/files.php
@@ -160,12 +160,14 @@ $project_id_url = $this->uri->segment(4);
                             $fileUrl = $file->path;
                         }
 
+                        $user_in_group = in_array($this->session->userdata("user_id"), explode(",", $group->owners));
+
                         ?>
 
                         <div class="file_list_item col-6 col-lg-3 mb-3" data-file-id="<?= $file->id ?>">
                             <div class="file-item border p-2 rounded-2">
                                 <div class="file-image" data-file-desc="<?= $file->description ?>" data-file-id="<?= $file->id ?>" data-file-type="<?= $file->type ?>" data-file-upload-date="<?= $file->created_at ?>" data-file-title="<?= $file->title ?>" data-file-path="<?= base_url($filePath . $filePathExplode[2]) ?>">
-                                    <?php if (!in_array($this->session->userdata("user_id"), explode(",", $group->owners))) : ?>
+                                    <?php if (!$user_in_group) : ?>
                                         <img src="<?= base_url($fileUrl) ?>" alt="file_icon">
                                     <?php else : ?>
                                         <?php if ($file->type == 'jpeg' || $file->type ==  'png' || $file->type == 'jpg') : ?>
@@ -176,7 +178,7 @@ $project_id_url = $this->uri->segment(4);
                                             <img src="<?= base_url("assets/images/file-icon.svg") ?>" alt="file_icon">
                                         <?php endif; ?>
                                     <?php endif; ?>
-                                    <div class="file-info" hidden data-file-url-download="<?= base_url($fileUrlDownload) ?>" data-file-desc="<?= $file->description ?>" data-file-id="<?= $file->id ?>" data-file-type="<?= $file->type ?>" data-file-upload-date="<?= $file->created_at ?>" data-file-title="<?= $file->title ?>" data-file-path="<?= base_url($fileUrl) ?>"></div>
+                                    <div class="file-info" data-test="<?= base_url($fileUrl) ?>"  data-check="<?= $user_in_group; ?>" hidden data-file-url-download="<?= base_url($fileUrlDownload) ?>" data-file-desc="<?= $file->description ?>" data-file-id="<?= $file->id ?>" data-file-type="<?= $file->type ?>" data-file-upload-date="<?= $file->created_at ?>" data-file-title="<?= $file->title ?>" data-file-path="<?= base_url($fileUrl) ?>"></div>
                                 </div>
 
                                 <div class="file-info">
